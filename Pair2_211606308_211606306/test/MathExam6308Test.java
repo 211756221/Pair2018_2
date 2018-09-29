e36b99b4e21e6de6fc917aed7f895b38139b527f
211756221 <43142589+211756221@users.noreply.github.com>
2018-09-30 00:35:45 +0800

    结对作业2
    测试类初步编写

---
+++ Pair2_211606308_211606306/test/MathExam6308Test.java
@@ -0,0 +1,28 @@
+package com.PSP6308;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathExam6308Test {
+
+	@Test
+	void testZzz() {
+		
+	}
+
+	@Test
+	void testXxx() {
+	}
+
+	@Test
+	void testTtt() {
+	}
+
+	@Test
+	void testRem() {
+		String[] strArr = {"1","3","+"};
+		assertEquals("4", MathExam6308.rem(strArr));
+	}
+
+}
